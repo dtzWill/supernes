77f0a29c0b4762168289325d66e5df8fa8cb214e
Javier S. Pedro <maemo@javispedro.com>
2009-08-18 13:16:40 +0200

    load/save implemented using gnomevfs

--- gui/save.c
+++ gui/save.c
@@ -24,17 +24,38 @@
 
 #include <string.h>
 #include <glib.h>
+#include <libgnomevfs/gnome-vfs-uri.h>
+#include <libgnomevfs/gnome-vfs-utils.h>
+#include <libgnomevfs/gnome-vfs-ops.h>
+#include <libgnomevfs/gnome-vfs-xfer.h>
 #include <hildon/hildon-file-chooser-dialog.h>
 
 #include "plugin.h"
 
-static gchar * cur_save_filename = NULL;
+static gchar * cur_save_uri = NULL;
+
+// Pulling nearly all of gnomevfs just to copy a file. *sigh*.
+static void copy_file(const char * source_uri, const char * dest_uri)
+{
+	GnomeVFSURI* src = gnome_vfs_uri_new(source_uri);
+	GnomeVFSURI* dst = gnome_vfs_uri_new(dest_uri);
+	GnomeVFSResult res;
+
+	res = gnome_vfs_xfer_uri(src, dst,
+			GNOME_VFS_XFER_TARGET_DEFAULT_PERMS,
+			GNOME_VFS_XFER_ERROR_MODE_ABORT,
+			GNOME_VFS_XFER_OVERWRITE_MODE_REPLACE,
+			NULL, NULL);
+
+	gnome_vfs_uri_unref(src);
+	gnome_vfs_uri_unref(dst);
+}
 
 void save_clear()
 {
-	if (cur_save_filename) {
-		g_free(cur_save_filename);
-		cur_save_filename = NULL;
+	if (cur_save_uri) {
+		g_free(cur_save_uri);
+		cur_save_uri = NULL;
 	}
 }
 
@@ -42,46 +63,72 @@ static gchar * show_dialog(GtkWindow* parent, GtkFileChooserAction action)
 {
 	GtkWidget * dialog;
 	GtkFileFilter * filter;
-	gchar * filename = NULL;
+	gchar * uri = NULL;
 
 	filter = gtk_file_filter_new();
 	gtk_file_filter_add_pattern(filter, "*.snsg");
 
 	dialog = hildon_file_chooser_dialog_new_with_properties(GTK_WINDOW(parent), 
-		"action", action, "filter", filter, NULL);
+		"action", action, "local-only", FALSE, "filter", filter, NULL);
 
 	// TODO Default path
 
 	gtk_widget_show_all(GTK_WIDGET(dialog));
 	if (gtk_dialog_run(GTK_DIALOG(dialog)) == GTK_RESPONSE_OK) {
-		filename = gtk_file_chooser_get_filename(GTK_FILE_CHOOSER(dialog));
+		uri = gtk_file_chooser_get_uri(GTK_FILE_CHOOSER(dialog));
 	}
 
 	gtk_widget_destroy(dialog);
 
-	return filename;
+	return uri;
 }
 
 void save_load(GtkWindow* parent)
 {
-	gchar * filename = show_dialog(parent, GTK_FILE_CHOOSER_ACTION_OPEN);
+	gchar * uri = show_dialog(parent, GTK_FILE_CHOOSER_ACTION_OPEN);
+
+	if (uri) {
+		gchar * frz_file = game_state_get_frz_file();
+		gchar * frz_uri = gnome_vfs_get_uri_from_local_path(frz_file);
+		copy_file(uri, frz_uri);
+		g_free(frz_uri);
+		g_free(frz_file);
+	}
 	
-	// TODO: Something
+	if (cur_save_uri) {
+		g_free(cur_save_uri);
+	}
+	cur_save_uri = uri;
 }
 
 void save_save(GtkWindow* parent)
 {
-	if (!cur_save_filename) {
+	if (cur_save_uri) {
+		gchar * frz_file = game_state_get_frz_file();
+		gchar * frz_uri = gnome_vfs_get_uri_from_local_path(frz_file);
+		copy_file(frz_uri, cur_save_uri);
+		g_free(frz_uri);
+		g_free(frz_file);
+	} else {
 		save_save_as(parent);
 	}
-	
-	// TODO: Something again
 }
 
 void save_save_as(GtkWindow* parent)
 {
-	gchar * filename = show_dialog(parent, GTK_FILE_CHOOSER_ACTION_SAVE);
-	
-	// TODO: Something again
+	gchar * uri = show_dialog(parent, GTK_FILE_CHOOSER_ACTION_SAVE);
+
+	if (uri) {
+		gchar * frz_file = game_state_get_frz_file();
+		gchar * frz_uri = gnome_vfs_get_uri_from_local_path(frz_file);
+		copy_file(frz_uri, uri);
+		g_free(frz_uri);
+		g_free(frz_file);
+
+		if (cur_save_uri) {
+			g_free(cur_save_uri);
+		}
+		cur_save_uri = uri;
+	}
 }
 
