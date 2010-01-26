e8310599833ba18558ab75b9334f45efc74cab6c
Javier S. Pedro <maemo@javispedro.com>
2010-01-26 20:47:00 +0100

    uploading i18n template

--- platform/osso.h
+++ platform/osso.h
@@ -1,19 +1,18 @@
-#ifndef _PLATFORM_HGW_H_
-#define _PLATFORM_HGW_H_
+#ifndef _PLATFORM_GUICONF_H_
+#define _PLATFORM_GUICONF_H_
 
-#ifdef __cplusplus
-/** True if we were launched from GUI. */
-extern bool hgwLaunched;
+START_EXTERN_C
 
 /** Called from main() before loading config; connects to DBus. */
-void HgwInit();
+void GuiConfInit();
 /** Called from main() before closing. */
 void HgwDeinit();
 /** Called from main() after loading user config; loads GUI settings. */
 void HgwConfig();
 /** Called from main() in the event loop; polls DBus. */
-void HgwPollEvents();
-#endif
+void GuiConfigPollEvents();
+
+END_EXTERN_C
 
 #endif
 
