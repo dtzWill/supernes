16924c125ee57f36f4ce125403fdef179706b757
Will Dietz <w@wdtz.org>
2010-09-03 19:28:20 -0500

    When use enables 'show skin', always show skin, regardless of orientation

--- platform/GLUtil.cpp
+++ platform/GLUtil.cpp
@@ -288,6 +288,10 @@ void updateOrientation()
     float screenAspect = (float)destWidth/(float)destHeight;
     float emulatedAspect = (float)srcWidth/(float)srcHeight;
     
+    //XXX: Hack until skins support setting their orientation
+    if ( use_on_screen && skin )
+      orientation = ORIENTATION_LANDSCAPE_R;
+    
     switch( orientation )
     {
         case ORIENTATION_LANDSCAPE_R:
@@ -315,7 +319,7 @@ void updateOrientation()
         vertexCoords[2*i+1] *= screenAspect / emulatedAspect;
     }
 
-    if ( use_on_screen && orientation == ORIENTATION_LANDSCAPE_R && skin )
+    if ( use_on_screen && skin )
     {
         float controller_aspect = (float)skin->controller_screen_width / (float)skin->controller_screen_height;
         float scale_factor;
@@ -413,7 +417,7 @@ void GL_RenderPix(u8 * pix)
     /*-----------------------------------------------------------------------------
      *  Background Skin
      *-----------------------------------------------------------------------------*/
-    if ( use_on_screen && orientation == ORIENTATION_LANDSCAPE_R && skin && !skin->transparent)
+    if ( use_on_screen && skin && !skin->transparent)
     {
       drawSkin();
     }
@@ -454,7 +458,7 @@ void GL_RenderPix(u8 * pix)
     /*-----------------------------------------------------------------------------
      *  Skin Overlay
      *-----------------------------------------------------------------------------*/
-    if ( use_on_screen && orientation == ORIENTATION_LANDSCAPE_R && skin && skin->transparent)
+    if ( use_on_screen && skin && skin->transparent)
     {
       drawSkin();
     }
